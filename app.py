6b05fcfc3d29d41808ed3a10b5b3e90e36d8b858
J03-H <joe.harland@education.nsw.gov.au>
2025-11-13 14:57:44 +1100

    adding waitress to the app file for online deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 import os
+from waitress import serve
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -53,4 +54,4 @@ def add_user():
         return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8000)
