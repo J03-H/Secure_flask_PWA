f7402c75e767a2fc9bf7ee828e52880407439e1a
J03-H <joe.harland@education.nsw.gov.au>
2025-11-13 14:52:57 +1100

    adding new users to the database to login to the web page

--- app.py
+++ app.py
@@ -23,13 +23,34 @@ def login_validation():
     else:
         return redirect('/')
     
-app.route('/home')
+@app.route('/home')
 def home():
     fname = request.args.get('fname')
     lname = request.args.get('lname')
     email = request.args.get('email')
 
     return render_template('home.html', fname=fname, lname=lname, email=email)
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    connection = sqlite3.connect('LoginData.db')
+    cursor = connection.cursor()
+
+    ans = cursor.execute("select * from USER where email=? AND password=?",(email, password)).fetchall()
+
+    if len(ans) > 0:
+        connection.close()
+        return render_template('login.html')
+    else:
+        cursor.execute("INSERT INTO USERS(fname,lname,email,password)value(?,?,?,?)",(fname,lname,email,password))
+        connection.commit()
+        connection.close()
+        return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
